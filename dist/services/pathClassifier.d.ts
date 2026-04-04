1793da749b1b703487c9d88dda82f8dc97d26f03
fox3000foxy <40730498+fox3000foxy@users.noreply.github.com>
2026-04-04 22:21:13 +0200

    cleaned repo files

--- dist/services/pathClassifier.d.ts
+++
@@ -1,9 +0,0 @@
-import { KnownPathOptions } from "../types";
-export interface KnownPathResult {
-    isKnown: boolean;
-    type: "api" | "page" | "unknown";
-}
-export declare class PathClassifier {
-    static normalizePath(inputPath: string): string;
-    static isKnownPath(inputPath: string, options: KnownPathOptions): KnownPathResult;
-}
--- dist/services/unhandledRoutesService.d.ts
+++
@@ -1,8 +0,0 @@
-import { KnownPathOptions } from "../types";
-import { TrafficService } from "./trafficService";
-export declare class UnhandledRoutesService {
-    private readonly trafficService;
-    private readonly listAvailableEndpoints;
-    constructor(trafficService: TrafficService, listAvailableEndpoints: () => string[]);
-    getUnhandledRoutes(additionalEndpoints: string[], knownPathOptions: KnownPathOptions): string[];
-}
